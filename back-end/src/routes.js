c79296ff79d2d8722953d34179567eaae2c10b23
Jonatas Da Silva <jonatas.publinet@gmail.com>
2020-04-14 09:32:38 -0300

    Update Backend

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -11,15 +11,7 @@ routes.post('/sessions', SessionController.create); //L ogin Ongs
 
 routes.get('/ongs', OngController.index); //List ongs
 
-routes.post('/ongs', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(11),
-    city: Joi.string().required(),
-    uf: Joi.string().required().length(2),
-  }), 
-}), OngController.create); //Create Ongs
+routes.post('/ongs', OngController.create); //Create Ongs
 
 routes.get('/incidents', celebrate({
   [Segments.QUERY]: Joi.object().keys({
